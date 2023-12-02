87d997287a46816730f177d4fabe41514397ae54
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2023-12-02 22:51:21 +0530

    Delete Add attribute to script tag directory

--- Add attribute to script tag/dom.js
+++
@@ -1,6 +0,0 @@
-function changeColor() {
-    var content = document.getElementById("content");
-    content.style.backgroundColor = "yellow";
-}
-  
-changeColor();
--- Add attribute to script tag/footer.js
+++
@@ -1,8 +0,0 @@
-function addYear() {
-    var year = new Date().getFullYear();
-    var footer = document.getElementById("footer");
-    footer.innerHTML += " " + year;
-}
-  
-addYear();
-  
